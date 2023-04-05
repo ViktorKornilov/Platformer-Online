9e36f74126a26ae2c97dc9a43afcf2b384b682f1
Vik <vik :)>
2023-04-05 18:02:37 +0300

    Imrpoved Jumping

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5;
     public float jumpSpeed = 10;
     public float jumpHeight = 3;
+    public LayerMask layerMask;
     
     public bool onGround;
     Rigidbody2D rb;
@@ -16,6 +17,10 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        var hit = Physics2D.Raycast(transform.position, Vector2.down,0.7f,layerMask);
+        onGround = hit.collider != null;
+        
+        
         if (onGround && Input.GetKeyDown(KeyCode.Space))
         {
             var speed = Mathf.Sqrt(jumpHeight * -rb.gravityScale * Physics2D.gravity.y * 2f);
@@ -25,15 +30,5 @@ public class Player : MonoBehaviour
 
 
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed,rb.velocity.y);
-    }
-
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        onGround = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D col)
-    {
-        onGround = false;
     }
 }
